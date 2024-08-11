df8890c0896b0fb431d19b92e71e33a240721d04
giancrafthd <gianlucamk04@gmail.com>
2024-08-10 21:50:29 -0300

    commitando

---
+++ src/testes/Operacao.java
@@ -0,0 +1,19 @@
+package testes;
+
+public class Operacao {
+	public double adicao(double n1, double n2) {
+		return n1 + n2;
+	}
+	
+	public double subtracao(double n1, double n2) {
+		return n1 - n2;
+	} 
+	
+	public double multiplicacao(double n1, double n2) {
+		return n1 * n2;
+	}
+	
+	public double divisao(double n1, double n2) {
+		return n1 / n2;
+	}
+}
